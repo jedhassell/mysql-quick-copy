8f09ffab7bcd13e70a4db88352fcc8de24da562c
jedhassell <jedhassell@gmail.com>
2017-12-14 13:33:57 -0800

    adding ability to import schema and data

--- import.rb
+++ import.rb
@@ -2,56 +2,59 @@ require 'mysql2'
 require 'slop'
 require 'byebug'
 require 'fileutils'
+require 'io/console'
 
 class Import
 
-  # get mysqls datadir
-  # source db
-  # username
-  # password
-  # destination
-
   def initialize
-    # puts ARGV
     source_dir = '/usr/local/var/mysql_blank'
     @database = 'real_store_development6'
-    # @destination = File.join(destination_datadir, @database)
-    @mysql = Mysql2::Client.new(:host => 'localhost', :username => 'root', :database => @database)
+    @db_files = File.join(source_dir, @database)
+    @mysql = Mysql2::Client.new(:host => 'localhost', :username => 'root')
+    @mysql.query("drop database if exists #{@database}") ############### REMOVE ME
+    @mysql.query("create database #{@database}")
+    @mysql.query("use #{@database}")
     @datadir = @mysql.query('select @@datadir;').first['@@datadir']
+
+    import_schema
+
     @tables = @mysql.query('show tables').map { |item| item.values.first }
 
-    FileUtils.mkdir_p(@destination)
+    @mysql.query('SET FOREIGN_KEY_CHECKS=0')
 
-    @tables.each do |table|
-      puts '.'
-      flush_table(table)
-      copy_table(table)
-      unlock_tables
+    spinner = ['—', '\\', '|', '/', '—', '\\', '|']
+
+    num_tables = @tables.size
+    @tables.each_with_index do |table, i|
+      print " [#{i}/#{num_tables}] complete.  #{spinner[i % spinner.size]}\r"
+      $stdout.flush
+      drop_tablespace(table)
+      copy_table_data(table)
+      import_tablespace(table)
     end
+
+    @mysql.query('SET FOREIGN_KEY_CHECKS=1')
   end
 
-  def flush_table(table)
-    @mysql.query("FLUSH TABLES #{table} FOR EXPORT")
+  def drop_tablespace(table)
+    @mysql.query("ALTER TABLE #{table} DISCARD TABLESPACE")
   end
 
-  def copy_table(table)
-    ['cfg', 'ibd'].each do |ext|
-      FileUtils.cp(File.join(@datadir, @database, "#{table}.#{ext}"), @destination)
-    end
+  def import_tablespace(table)
+    @mysql.query("ALTER TABLE #{table} IMPORT TABLESPACE")
   end
 
-  def unlock_tables
-    @mysql.query('unlock tables')
+  def import_schema
+    `mysql -u root #{@database} < #{File.join(@db_files, 'schema.sql')}`
+  end
+
+  def copy_table_data(table)
+    FileUtils.cp([File.join(@db_files, "#{table}.cfg"), File.join(@db_files, "#{table}.ibd")], File.join(@datadir, @database))
   end
 
   # def parse_options
   #   # use slop
   # end
-  #
-  # def dump_schema
-  #   `mysqldump -u root -p --no-data dbname > schema.sql`
-  # end
-
 end
 
 Import.new
